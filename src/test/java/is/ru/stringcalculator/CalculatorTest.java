f3dac5174cbca4673cbd583e9a459ea9ba8cf119
bjarnigud <bjarnigud13@ru.is>
2015-10-17 03:09:44 +0000

    Started from scratch in calculator and calculator test and finished first step again

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -9,24 +9,18 @@ public class CalculatorTest {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
-	@Test
-	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
-	}
-
-	@Test
-	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
-	}
-
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
-
-	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
+    @Test
+    public void testIfStringEmpty(){
+    	assertEquals(0, Calculator.add(""));
     }
 
+    @Test 
+    public void testIfOneNumber(){
+    	assertEquals(1,Calculator.add("1"));
+    }
+
+    @Test
+    public void testIfTwoNumbers(){
+    	assertEquals(3, Calculator.add("1,2"));
+    }
 }
